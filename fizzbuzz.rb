0711c43e27abd0b82f2d84ff4be9f057176efd77
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-01 17:22:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 15 == 0
+  if int % 3 == 0
     puts "FizzBuzz"
-  elsif int % 3 == 0
-    puts "Fizz"
   elsif int % 5 == 0
+    puts "Fizz"
+  elsif int % 15 == 0
     puts "Buzz"
   end
 end
