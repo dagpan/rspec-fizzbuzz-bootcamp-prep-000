256e5704ff06bb7ca5edff7afbb8a12d1fd276a5
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-01 17:48:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,15 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    return "Fizz"
-  elsif int % 5 == 0
+    if int % 5 == 0
+       return "FizzBuzz"
+    else
+       return "Fizz"
+    end
+   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 15 == 0
-    return "FizzBuzz"
+#  elsif int % 15 == 0
+#return "FizzBuzz"
   end
 end
 
